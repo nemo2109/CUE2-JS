3f13fbb2291259af18b5ae5c9871c3d2b41f1f7e
nemo2109 <114691858+nemo2109@users.noreply.github.com>
2022-12-07 12:07:43 -0600

    Funcionamiento finalizado

--- JS/index.js
+++ JS/index.js
@@ -3,15 +3,28 @@ const main = document.getElementById('menu');
 const contenidoInicial = document.getElementById('contenido-1');
 const mainBtn = document.getElementById('main-btn');
 
+
 // Constantes para almacenar contenido
 const contenido2 = document.getElementById('contenido-2');
+const contenidoNotas = document.getElementById('cont-notas')
+
+// Craer una variable global para almacenar elementos
+let mainForm;
+
+// Variables con forma globales
+let titulo;
+let nota;
+let fecha;
 
 main.addEventListener('click', (e) => {
   // Para confirmar si el click es el btn principal
   if (e.target === mainBtn) {
     contenidoInicial.remove();
 
-    contenido2.append(agregartNota);
+    contenido2.append(agregarNota);
+
+    // Asignar a mainForm el elemento de forma
+    mainForm = document.getElementById('main-form');
   }
 
   // Para confirmar si el button es de borrar
@@ -23,32 +36,65 @@ main.addEventListener('click', (e) => {
     }
   } 
 
-  // para encontrar el elemnto submit de la forma
+  // Para encontrar el elemento submit de la forma
   if (e.target.id === 'agregar-nota') {
-    const agregartNota = e.target;
-    console.log(agregartNota);
-
     // Crear escuchador de eventos para click en el buton agregar
-    agregartNota.addEventListener('click', (event) => {
-      // Crear constantes de los inputs para obbtener informacion de la forma
-      const titulo = document.getElementById('titulo');
-      const nota = document.getElementById('nota');
-      const fecha = document.getElementById('fecha');
-    } )
+    mainForm.addEventListener('submit', (event) =>{
+      // Evitar que la accion por defecto pase
+      event.preventDefault();
+
+      // Crear constantes de los inputs para obtener informacion de la forma
+      titulo = document.getElementById('titulo');
+      nota = document.getElementById('nota');
+      fecha = document.getElementById('fecha');
+
+      // Crear objeto para guardar informacion
+      const formulario = {
+        titulo1: titulo.value,
+        notaText: nota.value,
+        fecha1: fecha.value,
+      }
+
+      // Desconstruir el objeto
+      const { titulo1, notaText, fecha1 } = formulario;
+
+      // Creo constante con informacion
+      const notaHtmlConInfo = agregarNotaDinamico(titulo1, notaText, fecha1);
+
+      // Insertar HTML nuevo
+      contenidoNotas.append(notaHtmlConInfo);
+
+      // Reiniciar los valores de la forma 
+      titulo.value = '';
+      nota.value = '';
+      fecha = '';
+    });
+  
   }
 
 });
 
 // Crear elemento con HTML
-const agregartNota = document.createElement('div');
-agregartNota.innerHTML = `
-<form id="main.form">
+const agregarNota = document.createElement('div');
+agregarNota.innerHTML = `
+<form id="main-form">
     <input type="text" name="titulo" id="titulo" placeholder="Agregar titulo">
     <input type="text" name="nota" id="nota" placeholder="Agregar tu nota">
+    <input type="text" name="fecha" id="fecha" placeholder="Agregar la fecha de tu nota">
+
 
     <button type="button" id="borrar-nota">Borrar nota</button>
-    <button type="button" id="agregar-fecha">Agregar fecha</button>
+    <button type="submit" id="agregar-nota">Agregar nota</button>
   </form>
   `;
 
-  // crear una
+  // Funcion para agregar html dinamico en base a los resultados de la forma
+  function agregarNotaDinamico(titulo, nota, fecha) {
+    const notaEnHtml = document.createElement('div');
+    notaEnHtml.innerHTML = `
+    <h2>${titulo}</h2>
+    <p>${nota}</p>
+    <p>${fecha}</p>
+    `
+    return notaEnHtml;
+  }
